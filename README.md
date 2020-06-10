bc02c5e7ec8b958e308084a43d3a83f8286ac48b
Marisa Giancarla <marisag@empiredirectory.net>
2020-06-10 14:20:47 +0000

    Now uses the empire client to get the list

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # empusers
 Utilities for Wolfpack Empire operators
+
+Set up crontab like this:
+
+1 9,21 * * * /root/empusers/empusers.pl > /var/www/html/empusers.txt
+
--- empusers.pl
+++ empusers.pl
@@ -3,9 +3,9 @@
 # empusers.pl - Disdlay a list of user accounts still in sanctuary
 #               Outputs to stdout.
 
-my $UserOutputCmd = "echo sanc | nc synchronetbbs.org 6665";
-my @CountryNumbers = ();
+my $UserOutputCmd = "echo country \\* |/usr/local/bin/empire POGO peter";
 my @InputLines = ();
+my $CountryList = "";
 
 @InputLines = `$UserOutputCmd`;
 
@@ -13,14 +13,26 @@ foreach (@InputLines)
 {
 	my $row = $_;
 
+	#print "$row\n";
 	chomp $row;
-	if (substr($row, 0, 1) eq "1")
+	if (index($row, "Sanctuary") > 0)
 	{
-		my $ActualCountry = substr($row, 2);
-#		print "$ActualCountry\n";
-		push (@CountryNumbers, $ActualCountry);
+		my $ActualCountry = substr($row, 1);
+		#print "$ActualCountry\n";
+		my $countrynum = substr($row, 1, 3);
+		$countrynum =~ s/^\s+|\s+$//g;
+		# print "$countrynum\n";
+		if ($CountryList eq "")
+		{
+			$CountryList = $countrynum;
+		}
+		else
+		{
+			$CountryList = "$CountryList, $countrynum";
+		}
 	}
 }
-print "@CountryNumbers\n\n";
+print "Below are the countries still available to be claimed:\n";
+print "$CountryList\n\n";
 print "The password for each country is the same as the country number. Once you move out of your sanctuary you will \"own\" that country and can change the country name & password...\n";
 exit 0;
